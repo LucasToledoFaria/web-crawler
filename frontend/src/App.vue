0a399a62317131cf0c3820c7f859fda65dd9758b
LucasToledoFaria <lucastolefaria@gmail.com>
2025-02-19 14:35:55 -0300

    feat: add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
+  <div class="app-container">
+    <Header />
   </div>
 </template>
 
-
 <script>
+import Header from "./components/Structure/Header.vue";
+
 export default {
   name: "App",
+  components: {
+    Header,
+  },
   data() {
     return {
     };
   },
 };
 </script>
+
+<style scoped>
+.app-container {
+  padding-top: 4rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+</style>
