14eaaf88846649ccbeb058175bf6d57c4d814102
LucasToledoFaria <lucastolefaria@gmail.com>
2025-03-14 16:07:58 -0300

    refac: move search bar files

--- frontend/src/components/Crawler/CrawlerComponent.vue
+++ frontend/src/components/Crawler/CrawlerComponent.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import SearchBar from "./SearchBar.vue";
+import SearchBar from "./SearchBar/SearchBar.vue";
 import Links from "./Links/Links.vue";
 import DownloadButtons from "./DownloadButtons.vue";
 
